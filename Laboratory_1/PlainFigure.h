5849ef778a128a98ad609b5b5ca1bcabde68a22d
Błażej Pituła <blazej.pitula1@gmail.com>
2025-03-01 14:42:17 +0100

    Before task 1

---
+++ Laboratory_1/PlainFigure.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+class PlainFigure 
+{
+protected:
+ virtual void Print(std::ostream& out) const = 0;
+ friend std::ostream& operator<<(std::ostream& os, const PlainFigure&figure);
+public:
+ virtual double Area() = 0;
+ virtual double Perimeter() = 0;
+ virtual ~PlainFigure();
+};
